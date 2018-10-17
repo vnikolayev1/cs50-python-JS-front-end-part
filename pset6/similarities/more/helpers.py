d26985a2ce640b4431273813af959efc468129d0
Vasiliy <gigolas01@gmail.com>
2018-10-17 21:38:08 +0000

    upd similarities more

--- pset6/similarities/more/helpers.py
+++ pset6/similarities/more/helpers.py
@@ -14,6 +14,26 @@ class Operation(Enum):
 
 def distances(a, b):
     """Calculate edit distance from a to b"""
-
-    # TODO
-    return [[]]
+    # declaring empty cost 3d list
+    cost = [[(0, 0) for j in range(len(b) + 1)] for i in range(len(a) + 1)]
+    # getting through "one of empty cells scenario"
+    for i in range(1, len(b) + 1):
+        cost[0][i] = (i, Operation.INSERTED)
+    for i in range(1, len(a) + 1):
+        cost[i][0] = (i, Operation.DELETED)
+    # Figuring amount of possible operations
+    for i in range(1, len(a) + 1):
+        for j in range(1, len(b) + 1):
+            deletion_cost, _ = cost[i - 1][j]
+            deletion_cost += 1
+            insertion_cost, _ = cost[i][j - 1]
+            insertion_cost += 1
+            substraction_cost, _ = cost[i - 1][j - 1]
+            if not a[i - 1] == b[j - 1]:
+                substraction_cost += 1
+            # Creating list of cost operations to get best scenario out of it
+            cost_values = [(deletion_cost, Operation.DELETED), (insertion_cost,
+                                                                Operation.INSERTED), (substraction_cost, Operation.SUBSTITUTED)]
+            cost[i][j] = cost_values[cost_values.index(min(cost_values, key=lambda x: x[0]))]
+            # key = lambda x: x[0] will take first(0) value to compare
+    return(cost)
